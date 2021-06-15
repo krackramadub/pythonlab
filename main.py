416292ba2c181d80709c6b78b98883ca16f13437
krackramadub <radkevich.viktor83@gmail.com>
2021-06-16 00:59:43 +0300

    task1

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Hello world");
+# -*- coding: utf-8 -*-
+
+import random
+
+randomValue = random.randint(0, 100)
+
+print("Введите число от 0 до 100: ")
+inputValue = int(input())
+print("Случайное число: ", randomValue)
+
+if randomValue < inputValue:
+    print("Случайное число меньше")
+elif randomValue > inputValue:
+    print("Случайное число больше")
+else:
+    print("Числа равны")
